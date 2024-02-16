99b2cfd5b3d827173912775d115dae21fa274980
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-02-16 19:01:10 +0300

    feat: add solution to task filmography

---
+++ filmography/directors.js
@@ -0,0 +1,44 @@
+export const directors = [
+  {
+    id: 'stevenSpielberg',
+    name: 'Стивен Спилберг',
+    career: 'Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр',
+    filmography: 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0',
+    topRatedFilm: 'Список Шиндлера',
+  },
+  {
+    id: 'christopherNolan',
+    name: 'Кристофер Нолан',
+    career: 'Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор',
+    filmography: 'https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu',
+    topRatedFilm: 'Начало',
+  },
+  {
+    id: 'martinMcDonagh',
+    name: 'Мартин МакДона',
+    career: 'Сценарист, Режиссёр, Продюсер',
+    filmography: 'https://www.film.ru/person/martin-makdonah',
+    topRatedFilm: 'Три билборда на границе Эббинга, Миссури',
+  },
+  {
+    id: 'alexeyBalabanov',
+    name: 'Алексей Балабанов',
+    career: 'Режиссёр, Сценарист, Актёр, Продюсер',
+    filmography: 'https://www.film.ru/person/aleksey-balabanov',
+    topRatedFilm: 'Брат',
+  },
+  {
+    id: 'peterFarrelly',
+    name: 'Питер Фаррелли',
+    career: 'Продюсер, Режиссёр, Сценарист, Актёр',
+    filmography: 'https://www.film.ru/person/piter-farrelli',
+    topRatedFilm: 'Зелёная книга',
+  },
+  {
+    id: 'jeanMarcVallee',
+    name: 'Жан-Марк Валле',
+    career: 'Режиссер, Продюсер, Монтажёр, Актёр, Сценарист',
+    filmography: 'https://www.film.ru/person/zhan-mark-valle',
+    topRatedFilm: 'Далласский клуб покупателей',
+  },
+];
---
+++ filmography/script.js
@@ -0,0 +1,49 @@
+'use strict'
+import { directors } from './directors.js';
+
+const buttons = document.querySelector('.directors__container');
+const directorName = document.querySelector('.name');
+const directorCareer = document.querySelector('.career');
+const directorTopRatedFilm = document.querySelector('.topRatedFilm');
+const directorFilmography = document.querySelector('.filmography');
+const fade = document.querySelector('.fade');
+const popup = document.querySelector('.popup');
+const closeIcon = document.querySelector('.popup__close');
+const filmsButton = document.querySelector('.directors__best-films-button');
+const filmsContents = document.querySelector('.directors__best-films-list');
+
+function createPopup(directorCard) {
+  directorName.textContent = directorCard.name;
+  directorCareer.textContent = directorCard.career;
+  directorTopRatedFilm.textContent = directorCard.topRatedFilm;
+  directorFilmography.href = directorCard.filmography;
+}
+
+function close() {
+  fade.classList.remove('active');
+  popup.classList.remove('visible');
+}
+
+buttons.addEventListener('click', function(event) {
+
+  let director = event.target.closest('.directors__director');
+  if(!director) return;
+  let directorName = director.querySelector('.director').textContent;
+
+  let directorCard = directors.find((item) => item.name === directorName);
+
+  createPopup(directorCard);
+
+  fade.classList.add('active');
+  popup.classList.add('visible');
+
+  closeIcon.addEventListener('click', close);
+  fade.addEventListener('click', close);
+});
+
+function createBestFilms() {
+  const films = directors.map(item => item.topRatedFilm);
+  filmsContents.textContent = films.join(', ');
+}
+
+filmsButton.addEventListener('click', createBestFilms);
